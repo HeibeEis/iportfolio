f1f825265c3b072e6bd012284490290c1cfc61e0
HeibeEis <HeibeEis@outlook.com>
2024-07-19 02:10:23 -0400

    fix modulenotfounderror

--- table_page.py
+++ table_page.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
-from chat_openai import chat_with_openai
 
 from gpt_response import fetch_gpt4_response
 
